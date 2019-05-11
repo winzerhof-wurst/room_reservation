5e58a35c049107a73a0c903e03f81a14f441fdb1
Christoph Wurst <christoph@winzerhof-wurst.at>
2019-05-12 00:25:54 +0200

    Add a calendar view
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -1,19 +1,46 @@
 <template>
 	<Content app-name="room_reservation">
 		<Navigation></Navigation>
+		<AppContent>
+			<FullCalendar :config="config" :events="events"></FullCalendar>
+		</AppContent>
 	</Content>
 </template>
 
 <script>
+	import AppContent from 'nextcloud-vue/dist/Components/AppContent'
 	import Content from 'nextcloud-vue/dist/Components/Content'
+	import 'fullcalendar/dist/fullcalendar.css'
+	import {FullCalendar} from 'vue-full-calendar'
 
 	import Navigation from '../components/Navigation'
 
 	export default {
 		name: "Calendar",
 		components: {
+			AppContent,
 			Content,
+			FullCalendar,
 			Navigation,
+		},
+		computed: {
+			config () {
+				return {
+					editable: false,
+					header: false,
+					selectable: false,
+					weekends: false,
+				}
+			},
+			events () {
+				return [
+					{
+						title: 'Test',
+						start: '2019-05-11',
+						end: '2019-05-17',
+					}
+				]
+			},
 		}
 	}
 </script>
